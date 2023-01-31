d2e857a3856507e99b3b5e8b80e8ff4326dbe196
Miguel Angel Chorat Delgado <43972532+Miguelchorat@users.noreply.github.com>
2023-01-31 19:29:44 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import Home from './components/Home.vue'
+  import Header from './components/Header.vue'
+  
+  // let search = null
+</script>
+
+<template>
+  <Header search="search"/>
+  <header>
+    <div class="wrapper">
+      <Home />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+    // defineProps({
+    //     search: {
+    //         type: String,
+    //         required: false
+    //     }
+    // })
+</script>
+
+<template>
+    <header class="header">
+        <a href="#" class="header__identity">
+            <img class="header__identity__logo" src="./assets/img/logo.svg" alt="Imagen del logo">
+            <p class="header__identity__name">VIJU</p>
+        </a>        
+        <form class="header__browser">
+            <span class="header__browser__icon material-symbols-outlined">search</span>
+            <input class="header__browser__input" placeholder="Buscar..." :value="search"/>                                            
+        </form>
+        <nav class="header__nav">
+            <ul class="header__nav__list">
+                <li class="header__nav__list__item"><a href="#">LOG IN</a></li>
+                <li class="header__nav__list__item"><a href="#">SIGN IN</a></li>
+            </ul>
+        </nav>
+        <a class="header__menu--disabled" href="#">
+            <span class="header__menu__icon material-symbols-outlined">menu</span>
+        </a>
+    </header>
+</template>
