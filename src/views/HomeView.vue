80762e07dfd70b481abf2bfaece7aa2f6c6f59d0
Miguel Angel Chorat Delgado <43972532+Miguelchorat@users.noreply.github.com>
2023-02-03 17:53:27 +0100

    Retoques en la imagen para difuminar el borde inferior de la imagen.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,38 +5,43 @@
   <main class="main">
     <h1 class="main__title">ULTIMAS RESEÑAS</h1>
     <div class="main__reviews">
-      <a href="#" class="main__reviews__review">
-        <img class="main__reviews__review__img" src="../assets/img/logo.svg" alt="">
-        <h2 class="main__reviews__review__title">POKÉMON</h2>
-        <p class="main__reviews__review__user">@User</p>
+      <a href="#" class="main__reviews__review">        
+        <img class="main__reviews__review__img" src="../assets/img/placeholder.png" alt=""/>      
+        <h2 class="main__reviews__review__title">POKÉMON or Digimon</h2>
+        <p class="main__reviews__review__description">Esto es una descripción bastante escueta para ser probado...</p>
+        <p class="main__reviews__review__user">@MiguelChorat</p>
         <p class="main__reviews__review__score">0/5</p>        
       </a>
 
       <!-- PRUEBAS -->
-      <a href="#" class="main__reviews__review">
-        <img class="main__reviews__review__img" src="../assets/img/logo.svg" alt="">
-        <h2 class="main__reviews__review__title">POKÉMON</h2>
+      <span href="#" class="main__reviews__review">
+        <img class="main__reviews__review__img" src="../assets/img/placeholder.png" alt=""/>      
+        <h2 class="main__reviews__review__title">Jump King is awesome</h2>
+        <p class="main__reviews__review__description">Esto es una descripción bastante escueta para ser probado...</p>
         <p class="main__reviews__review__user">@User</p>
         <p class="main__reviews__review__score">0/5</p>        
-      </a>
-      <a href="#" class="main__reviews__review">
-        <img class="main__reviews__review__img" src="../assets/img/logo.svg" alt="">
-        <h2 class="main__reviews__review__title">POKÉMON</h2>
+      </span>
+      <span href="#" class="main__reviews__review">
+        <img class="main__reviews__review__img" src="../assets/img/placeholder.png" alt="">
+        <h2 class="main__reviews__review__title">Prueba prueba</h2>
+        <p class="main__reviews__review__description">Esto es una descripción bastante escueta para ser probado...</p>
         <p class="main__reviews__review__user">@User</p>
         <p class="main__reviews__review__score">0/5</p>        
-      </a>
-      <a href="#" class="main__reviews__review">
-        <img class="main__reviews__review__img" src="../assets/img/logo.svg" alt="">
-        <h2 class="main__reviews__review__title">POKÉMON</h2>
+      </span>
+      <span href="#" class="main__reviews__review">
+        <img class="main__reviews__review__img" src="../assets/img/placeholder.png" alt="">
+        <h2 class="main__reviews__review__title">Esto es un ejemplo de prueba</h2>
+        <p class="main__reviews__review__description">Esto es una descripción bastante escueta para ser probado...</p>
         <p class="main__reviews__review__user">@User</p>
         <p class="main__reviews__review__score">0/5</p>        
-      </a>
-      <a href="#" class="main__reviews__review">
-        <img class="main__reviews__review__img" src="../assets/img/logo.svg" alt="">
-        <h2 class="main__reviews__review__title">POKÉMON</h2>
+      </span>
+      <span href="#" class="main__reviews__review">
+        <img class="main__reviews__review__img" src="../assets/img/placeholder.png" alt="">
+        <h2 class="main__reviews__review__title">Esto es otra prueba</h2>
+        <p class="main__reviews__review__description">Esto es una descripción bastante escueta para ser probado...</p>
         <p class="main__reviews__review__user">@User</p>
         <p class="main__reviews__review__score">0/5</p>        
-      </a>
+      </span>
     </div>
   </main>
 </template>
