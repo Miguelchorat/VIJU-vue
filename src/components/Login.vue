c3896d5725d6683a0e2dd731ca4b57d2e6708ce8
Miguelchorat <miguelchorat@gmail.com>
2023-02-07 22:22:34 +0100

    Cambios en las validaciones de login y signin

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,7 +9,9 @@ export default {
             errorEmail: false,
             errorPassword: false,
             remember: false,
-            visibility: false
+            visibility: false,
+            emailValidation: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            passwordValidation: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$/
         }
     },
     methods: {
@@ -20,7 +22,24 @@ export default {
             this.visibility = !this.visibility
         },
         checkFields(){
-            
+            let check = 0
+            this.errorEmail = false
+            this.errorPassword = false
+
+            if(this.emailValidation.test(this.email)){
+                check++
+            } else{
+                this.errorEmail = true
+            }
+
+            if(this.passwordValidation.test(this.password)){
+                check++   
+            } else {
+                this.errorPassword = true
+            }
+
+            if(check == 2)
+                alert("Validación completa")
         }
     }
 }
@@ -36,7 +55,7 @@ export default {
             <p class="session__field__error" :class="{ session__field__error__active: errorEmail }">El correo no es válido</p>
         </div>
         <div class="session__field">
-            <input class="session__field__input" :class="{ session__field__input__warning: errorPassword }" 
+            <input class="session__field__input" :class="{ session__field__input__warning: errorPassword }" :value="password" @input="event => password = event.target.value"
                 placeholder="Clave" :type="visibility ? 'text' : 'password'" aria-label="password" required maxlength="20"/>
             <span class="session__field__icon material-symbols-outlined" @click="listenVisibility">{{this.visibility ? 'visibility_off' : 'visibility'}}</span>
             <span class="session__field__square" :class="{ session__field__square__active: errorPassword }" />
@@ -48,7 +67,7 @@ export default {
                 class="session__remember__checkbox"/>
             <label class="session__remember__label">Recordarme</label>
         </div>
-        <button class="session__button" type="submit">INICIAR SESIÓN</button>
+        <button class="session__button" type="button" @click="checkFields">INICIAR SESIÓN</button>
         <div class="session__signin">
             <label class="session__signin__label">¿No tienes cuenta?</label>
             <a href="#" class="session__signin__link" @click="() => this.$emit('listenMenu', 2)">Create una</a>
--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -4,10 +4,23 @@ export default {
 
     data() {
         return {
+            email: '',
+            password: '',
+            repeatPassword: '',
+            username: '',
+            date: '',
             errorEmail: false,
             errorPassword: false,
+            errorRepeatPassword: false,
+            errorUsername: false,
+            errorDate: false,
             remember: false,
-            visibility: false
+            visibility: false,
+            emailValidation: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            passwordValidation: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$/,
+            usernameValidation: /^[a-zA-Z]{3,18}$/,
+            MIN_AGE_LIMIT: new Date(Date.now()).getFullYear()-13,
+            MAX_AGE_LIMIT: new Date(Date.now()).getFullYear()+120
         }
     },
     methods: {
@@ -16,6 +29,47 @@ export default {
         },
         listenVisibility() {
             this.visibility = !this.visibility
+        },
+        checkFields(){
+            let check = 0
+            this.errorEmail = false
+            this.errorPassword = false
+            this.errorRepeatPassword = false
+            this.errorDate = false
+            this.errorUsername = false
+
+            if(this.emailValidation.test(this.email)){
+                check++
+            } else{
+                this.errorEmail = true
+            }
+
+            if(this.usernameValidation.test(this.username)){
+                check++   
+            } else {
+                this.errorUsername = true
+            }
+
+            if(this.passwordValidation.test(this.password)){
+                check++   
+            } else {
+                this.errorPassword = true
+            }
+
+            if(this.repeatPassword !== "" && this.repeatPassword === this.password){
+                check++   
+            } else {
+                this.errorRepeatPassword = true
+            }
+
+            if(this.date!=='' && (new Date(this.date).getFullYear() < this.MIN_AGE_LIMIT || new Date(this.date).getFullYear() > this.MAX_AGE_LIMIT)){
+                check++   
+            } else {
+                this.errorDate = true
+            }
+
+            if(check == 5)
+                alert("Validación completa")
         }
     }
 }
@@ -26,19 +80,19 @@ export default {
         <a class="session__close material-symbols-outlined" href="#" @click="() => this.$emit('listenMenu')">close</a>
         <h2 class="session__title">REGISTRARSE</h2>
         <div class="session__field">
-            <input class="session__field__input" type="text"
-                placeholder="Nombre de usuario" aria-label="username" :class="{ session__field__input__warning: errorEmail }" required />
-            <span class="session__field__square" :class="{ session__field__square__active: errorEmail }" />
-            <p class="session__field__error" :class="{ session__field__error__active: errorEmail }">Nombre de usuario no válido</p>
+            <input class="session__field__input" type="text" :value="username" @input="event => username = event.target.value"
+                placeholder="Nombre de usuario" aria-label="username" :class="{ session__field__input__warning: errorUsername }" required />
+            <span class="session__field__square" :class="{ session__field__square__active: errorUsername }" />
+            <p class="session__field__error" :class="{ session__field__error__active: errorUsername }">Nombre de usuario no válido</p>
         </div>
         <div class="session__field">
             <input class="session__field__input" :class="{ session__field__input__warning: errorEmail }" type="email"
-                placeholder="Correo" aria-label="email" required />
+                placeholder="Correo" aria-label="email" required :value="email" @input="event => email = event.target.value"/>
             <span class="session__field__square" :class="{ session__field__square__active: errorEmail }" />
             <p class="session__field__error" :class="{ session__field__error__active: errorEmail }">El correo no es válido</p>
         </div>
         <div class="session__field">
-            <input class="session__field__input" :class="{ session__field__input__warning: errorPassword }" 
+            <input class="session__field__input" :class="{ session__field__input__warning: errorPassword }" :value="password" @input="event => password = event.target.value"
                 placeholder="Clave" :type="visibility ? 'text' : 'password'" aria-label="password" required maxlength="32"/>
             <span class="session__field__icon material-symbols-outlined" @click="listenVisibility">{{this.visibility ? 'visibility_off' : 'visibility'}}</span>
             <span class="session__field__square" :class="{ session__field__square__active: errorPassword }" />
@@ -46,18 +100,18 @@ export default {
                 válida</p>
         </div>
         <div class="session__field">
-            <input class="session__field__input" :class="{ session__field__input__warning: errorPassword }" 
+            <input class="session__field__input" :class="{ session__field__input__warning: errorRepeatPassword }" :value="repeatPassword" @input="event => repeatPassword = event.target.value"
                 placeholder="Repetir clave" :type="visibility ? 'text' : 'password'" aria-label="repeat-password" required maxlength="32"/>
             <span class="session__field__icon material-symbols-outlined" @click="listenVisibility">{{this.visibility ? 'visibility_off' : 'visibility'}}</span>
             <span class="session__field__square" :class="{ session__field__square__active: errorRepeatPassword }" />
             <p class="session__field__error" :class="{ session__field__error__active: errorRepeatPassword }">Las contraseñas no son iguales</p>
         </div>
         <div class="session__field">
-            <input class="session__field__input" type="date" aria-label="date"/>            
+            <input class="session__field__input" type="date" :class="{ session__field__input__warning: errorDate }" aria-label="date" :value="this.date" @input="event => this.date = event.target.value"/>            
             <span class="session__field__square" :class="{ session__field__square__active: errorDate }" />
             <p class="session__field__error" :class="{ session__field__error__active: errorDate }">La fecha no es válida</p>
         </div>
-        <button class="session__button" type="submit">INICIAR SESIÓN</button>
+        <button class="session__button" type="button" @click="checkFields">REGISTRARSE</button>
         <div class="session__signin">
             <label class="session__signin__label">¿Ya tienes una cuenta?</label>
             <a href="#" class="session__signin__link" @click="() => this.$emit('listenMenu', 1)">Iniciar sesión</a>
